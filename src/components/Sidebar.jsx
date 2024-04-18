1997a3d9384c5f77e445c83f654e732fdd722ebc
rahfimuhammad <rahfi.muhammad@alumni.ui.ac.id>
2024-04-18 16:06:37 +0700

    disabled button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,31 +18,32 @@ const Sidebar = () => {
         {isEditing
         ? <CreateConversation/>
         : <ChatList/>}
-        <div className='toggle-sidebar'
-             onClick={() => setToggle(!toggle)}
-             style={{
-                      width: "50px",
-                      height: "50px",
-                      position: "absolute",
-                      top: "calc(50% - 25px)",
-                      borderRadius: "50%",
-                      right: "-25px"
-                    }}>
-                      <CaretRight 
-                              style={{position: "absolute", 
-                                      top: "calc(50% - 10px)", 
-                                      left: "calc(100% - 25px"}} 
-                              size={20} 
-                              color='white' 
-                      />
-                      <CaretLeft 
-                              style={{position: "absolute", 
-                                      top: "calc(50% - 10px)", 
-                                      right: "calc(100% - 25px"}} 
-                              size={20} 
-                              color='white' 
-                      />
-                    </div>
+        <div 
+          className='toggle-sidebar'
+          onClick={() => setToggle(!toggle)}
+          style={{
+                  width: "50px",
+                  height: "50px",
+                  position: "absolute",
+                  top: "calc(50% - 25px)",
+                  borderRadius: "50%",
+                  right: "-25px"
+        }}>
+          <CaretRight 
+            style={{position: "absolute", 
+                    top: "calc(50% - 10px)", 
+                    left: "calc(100% - 25px"}} 
+            size={20} 
+            color='white' 
+          />
+          <CaretLeft 
+            style={{position: "absolute", 
+                    top: "calc(50% - 10px)", 
+                    right: "calc(100% - 25px"}} 
+            size={20} 
+            color='white' 
+          />
+        </div>
     </div>
   )
 }
