ecd1a23102c2daa79f260ae520a38b8011a785be
rahfimuhammad <rahfi.muhammad@alumni.ui.ac.id>
2024-04-23 19:13:33 +0700

    input ref

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import { useSocket } from '../context/SocketProvider'
 import { useContacts } from '../context/ContactsProvider'
 import { useApp } from '../context/AppProvider'
