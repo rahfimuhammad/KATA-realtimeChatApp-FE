f76808d753016f69f8e9cd6972fc33f8fe9f70e3
rahfimuhammad <rahfi.muhammad@alumni.ui.ac.id>
2024-07-13 19:53:12 +0700

    update conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,54 +1,48 @@
 import Auth from './components/Auth'
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { SocketProvider } from './context/SocketProvider';
 import { ContactsProvider } from './context/ContactsProvider';
 import { ConversationsProvider } from './context/ConversationsProvider';
 import Dashboard from './components/Dashboard';
 import { useApp } from './context/AppProvider';
-// import Loading from './components/Loading';
+import Loading from './components/Loading';
 
 function App() {
-
   const { id } = useApp()
-  // const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-  if(!id) {
-    return (
-      <Auth />
-    )
+  const connectServer = async () => {
+    setLoading(true)
+    try {
+        await axios.get('https://kata-realtimechatapp-be.onrender.com')
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
   }
 
-  // const connectServer = async () => {
-  //   try {
-  //     setLoading(true)
-  //     await axios.get('https://kata-realtimechatapp-be.onrender.com')  
-      
-  //   } catch (error) {
-  //     console.log(error)
-  //   } finally {
-  //     setLoading(false)
-  //   }
-  // }
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  // useEffect(() => {
-  //   if(id) {
-  //     connectServer()
-  //   }
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [id])
+  useEffect(() => {
+    if (id) {
+      connectServer()
+    }
+  }, [id])
 
   return (
     <>
-      <SocketProvider>
-        <ContactsProvider>
-          <ConversationsProvider id={id}>
-            {/* {loading && <Loading/>} */}
-            <Dashboard/>
-          </ConversationsProvider>
-        </ContactsProvider>
-      </SocketProvider>
+      {!id ? (
+        <Auth />
+      ) : (
+        <SocketProvider>
+          <ContactsProvider>
+            <ConversationsProvider id={id}>
+              {loading && <Loading />}
+              <Dashboard />
+            </ConversationsProvider>
+          </ContactsProvider>
+        </SocketProvider>
+      )}
     </>
   );
 }
